ad300ca86b83ce544e2a7cf77e1635e5a98f510c
Michael Nikolashin <perpendikulyar@gmail.com>
2024-07-23 20:42:15 +0300

    update userlink DTO

--- core/dto/userlink.dto.ts
+++ core/dto/userlink.dto.ts
@@ -2,9 +2,16 @@ import { DTO } from './dto';
 
 export class UserLink extends DTO {
     readonly tableName: string = 'links';
-    readonly savedProps: string[] = ['username', 'link'];
+    readonly savedProps: string[] = ['username', 'link', 'dateTitme'];
+    private dateTitme: Date;
     private username?: string;
     private link?: string;
+
+    constructor () {
+        super();
+        this.dateTitme = new Date();
+    }
+    
 
     static createAndSave(username: string, link: string) {
         const rec = new UserLink();
