e57867e1ffdffc35a83f9dbfde5aaacd804c43d0
Michael Nikolashin <mnikolashin@woman.ru>
2026-05-19 20:03:12 +0300

    fix

--- core/services/link.service.ts
+++ core/services/link.service.ts
@@ -18,8 +18,7 @@ export class LinkService {
         // Экранируем только значение utm_source, так как оно поступает от пользователя
         const safeRef = encodeURIComponent(ref);
         const safeSpec = encodeURIComponent(spec);
-        const safeBaseUrl = encodeURIComponent(baseUrl);
-        const url = `${safeBaseUrl}?utm_medium=${UTM_MEDIUM}&utm_source=${safeRef}&utm_campaign=${UTM_CAMPAIGN}`;
+        const url = `${baseUrl}?utm_medium=${UTM_MEDIUM}&utm_source=${safeRef}&utm_campaign=${UTM_CAMPAIGN}`;
         if (spec) {
             return await this.minify(`${url}&utm_term=${safeSpec}`);
         }
